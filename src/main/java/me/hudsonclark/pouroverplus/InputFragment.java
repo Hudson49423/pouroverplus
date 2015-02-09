46ece79a6e3fdcab22cb7f110b4b1d8c7d70a9f6
Hudson Clark <Hudson49423@gmail.com>
2015-02-09 18:19:14 -0500

    Added fuantion to cups up and down buttons

--- src/main/java/me/hudsonclark/pouroverplus/InputFragment.java
+++ src/main/java/me/hudsonclark/pouroverplus/InputFragment.java
@@ -1,18 +1,24 @@
 package me.hudsonclark.pouroverplus;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class InputFragment extends Fragment {
+public class InputFragment extends Fragment implements View.OnClickListener {
 
-    private EditText coffee;
-    private EditText cups;
+    private double numCups;
+    private int numCoffee;
+    private Button cupsUp;
+    private Button cupsDown;
+    private Button coffeeDown;
+    private Button coffeeUp;
+    private TextView cupsText;
+    private TextView coffeeText;
 
     public InputFragment() {
 
@@ -29,6 +35,8 @@ public class InputFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.input, container, false);
 
+        setUp(rootView);
+
         return rootView;
     }
 
@@ -38,8 +46,48 @@ public class InputFragment extends Fragment {
     }
 
 
+    private void setUp(View view) {
+        try {
+            numCups = Double.parseDouble(((TextView) view.findViewById(R.id.cups)).getText().toString());
+            numCoffee = Integer.parseInt(((TextView) view.findViewById(R.id.coffee)).getText().toString());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        cupsUp = (Button) view.findViewById(R.id.cups_up);
+        cupsDown = (Button) view.findViewById(R.id.cups_down);
+        coffeeUp = (Button) view.findViewById(R.id.coffee_up);
+        coffeeDown = (Button) view.findViewById(R.id.coffee_down);
 
-    private void changeCoffee() {}
+        cupsUp.setOnClickListener(this);
+        cupsDown.setOnClickListener(this);
+        coffeeDown.setOnClickListener(this);
+        coffeeUp.setOnClickListener(this);
 
-    private void changeCups() {}
+        cupsText = (TextView) view.findViewById(R.id.cups);
+        coffeeText = (TextView) view.findViewById(R.id.coffee);
+
+        numCups = 1;
+        numCoffee = 19;
+
+        cupsText.setText("" + numCups);
+        coffeeText.setText("" + numCoffee);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == cupsUp) {
+            numCups += 0.5;
+            numCoffee = (int) Math.round(numCups * 300 / 16);
+            coffeeText.setText("" + numCoffee);
+            cupsText.setText("" + numCups);
+        }
+        else if (v == cupsDown && numCups > 0) {
+            numCups -= 0.5;
+            numCoffee = (int) Math.round(numCups * 300 / 16);
+            coffeeText.setText("" + numCoffee);
+            cupsText.setText("" + numCups);
+        }
+    }
+
 }
